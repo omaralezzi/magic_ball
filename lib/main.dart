7a61f3875bc598b9174f17d77b1777f09f31124d
Omar Al-Ezzi <81753019+omaralezzi@users.noreply.github.com>
2024-02-24 03:27:46 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Ball(), // Using the Ball Stateful Widget here
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+      child: Center(
+        child:
+            Image.asset('images/ball1.png'), // Displaying the ball1.png image
+      ),
+    );
+  }
+}
